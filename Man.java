518a16d280d62c5f80e5d46d0a68ba553bd1c8e0
Mark <n.marik1990@gmail.com>
2025-11-02 12:02:57 +0300

    initial commit

---
+++ Man.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Man {
+        public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+                Random rand = new Random();
+                int x = rand.nextInt(10);
+                System.out.println(x);
+                System.out.println("Вам понравилось это число?");
+
+                String a = scanner.nextLine();
+                if (a == "да")
+                        System.out.println("отлично");
+                else if (a == "нет")
+                        System.out.println("печально");
+                else
+                        System.out.println("Вы ввели: " + a);
+                System.out.println("разговаривать научись мудень");
+        }
+}
